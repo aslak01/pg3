108813885ee4fff58bc272204e610783ce338d89
Aslak <hei@asla.cc>
2025-12-11 14:15:45 +0100

    fix clipboard

--- lib/clipboard.ml
+++ lib/clipboard.ml
@@ -1,13 +1,30 @@
-let copy_to_clipboard text =
-  let is_macos =
+let is_macos =
+  lazy (
     let ic = Unix.open_process_in "uname -s" in
     let os = input_line ic in
     close_in ic;
     os = "Darwin"
+  )
+
+let copy_to_clipboard text =
+  let clipboard_cmd =
+    if Lazy.force is_macos then "pbcopy"
+    else "xclip -selection clipboard"
   in
-  let cmd =
-    if is_macos then Printf.sprintf "printf '%%s' %s | pbcopy" (Filename.quote text)
-    else Printf.sprintf "printf '%%s' %s | xclip -selection clipboard" (Filename.quote text)
+  (* Write text to a temp file since we can't use pipes with spawn *)
+  let tmp = Filename.temp_file "clip" ".txt" in
+  let oc = open_out_bin tmp in
+  output_string oc text;
+  close_out oc;
+  (* Use Spawn which uses vfork (safe with OCaml 5 domains).
+     SIGCHLD is blocked globally at startup, so no signal will interrupt poll(). *)
+  let cmd = Printf.sprintf "cat %s | %s; rm -f %s" (Filename.quote tmp) clipboard_cmd (Filename.quote tmp) in
+  let pid = Spawn.spawn ~prog:"/bin/sh" ~argv:[ "/bin/sh"; "-c"; cmd ] () in
+  (* Wait for child - SIGCHLD is blocked so waitpid won't be interrupted *)
+  let rec wait () =
+    match Unix.waitpid [] pid with
+    | _, Unix.WEXITED 0 -> Ok ()
+    | _, _ -> Error "Clipboard command failed"
+    | exception Unix.Unix_error (Unix.EINTR, _, _) -> wait ()
   in
-  let exit_code = Unix.system cmd in
-  match exit_code with Unix.WEXITED 0 -> Ok () | _ -> Error "Failed to copy to clipboard"
+  wait ()
